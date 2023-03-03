1e2ae08a77f5c037c06d727d8c47a908681b7f89
Mika Rinne <mika.rinne@oracle.com>
2023-03-03 19:58:20 +0200

    fixes

--- scanning-readq/scan.sh
+++ scanning-readq/scan.sh
@@ -1,24 +1,19 @@
-OCI_CLI_AUTH=instance_principal
-
 /root/bin/oci -v
-/root/bin/oci os ns get
-uvscan --version
-
-exit
-
+/root/bin/oci os ns get --auth instance_principal
+#uvscan --version
 rm -f report.txt
 rm -rf scandir
 mkdir scandir
 echo "Scanning $1"
-/root/bin/oci os get --bucket-name scanning-ms --name $1 --region eu-amsterdam-1 --file scandir/$1
+/root/bin/oci os get --bucket-name scanning-ms --name $1 --region eu-amsterdam-1 --file scandir/$1 --auth instance_principal
 uvscan -v --unzip --analyze --summary --afc 512 --program --mime --recursive --threads=$(nproc) --report=report.txt --rptall --rptcor --rpterr --rptobjects scandir
 isInFile=$(cat report.txt | grep -c "Possibly Infected:.............     0")
 if [ $isInFile -eq 0 ]; then
    echo "################# ALERT!!! Scanning found infected files ! #################"
-   /root/bin/oci os object put --bucket-name scanning-ms-alert-report --region eu-amsterdam-1 --file report.txt --force
+   /root/bin/oci os object put --bucket-name scanning-ms-alert-report --region eu-amsterdam-1 --file report.txt --force --auth instance_principal
 else
    echo "################# Scanning found no infected files #################"
-   /root/bin/oci os object put --bucket-name scanned-ms --region eu-amsterdam-1 --file report.txt --force
-   /root/bin/oci os object put --bucket-name scanned-ms --region eu-amsterdam-1 --file scandir/$1 --force
+   /root/bin/oci os object put --bucket-name scanned-ms --region eu-amsterdam-1 --file report.txt --force --auth instance_principal
+   /root/bin/oci os object put --bucket-name scanned-ms --region eu-amsterdam-1 --file scandir/$1 --force --auth instance_principal
 fi
-/root/bin/oci os object delete --bucket-name scanning-ms --object-name $1 --region eu-amsterdam-1 --force
+/root/bin/oci os object delete --bucket-name scanning-ms --object-name $1 --region eu-amsterdam-1 --force --auth instance_principal
