f8c603fb50ff37d43125e71d6296b9b9aed0e541
Mika Rinne <mika.rinne@oracle.com>
2023-03-03 10:33:15 +0200

    fix

--- scanning-writeq/func.js
+++ scanning-writeq/func.js
@@ -1,10 +1,42 @@
-const fdk=require('@fnproject/fdk');
+const fdk = require('@fnproject/fdk');
+const core = require("oci-core");
+const identity = require("oci-identity");
+const common = require("oci-common");
+const os = require("oci-objectstorage");
+const queue = require("oci-queue");
 
-fdk.handle(function(input){
-  let name = 'World';
-  if (input.name) {
-    name = input.name;
-  }
-  console.log('\nInside Node Hello World function')
-  return {'message': 'Hello ' + name}
+fdk.handle(async function(event) {
+    
+    var res = "";
+    const queueId = 'ocid1.queue.oc1.eu-amsterdam-1.amaaaaaauevftmqa4er5filmkshxfpad2leyurhw7t7ilg4txundaly6g7ba';
+    const endpoint = 'https://cell-1.queue.messaging.eu-amsterdam-1.oci.oraclecloud.com';    
+
+    console.log(event);
+    
+    /*
+    try { 
+        console.log(queueId);
+        
+        const provider = await common.ResourcePrincipalAuthenticationDetailsProvider.builder();
+        const qClient = new queue.QueueClient({ authenticationDetailsProvider: provider });
+        qClient.endpoint = endpoint;
+
+        console.log("Writing event to Queue .. ");
+        const putReq = {
+          queueId: queueId,
+          putMessagesDetails: { messages : [ { content: content } ] }
+        };
+        
+        const putRes = await client.putMessages(putReq);
+        console.log(putRes);    
+        
+    } catch (error) {
+        console.log("Error: " + error);
+        res = "error " + error + ", queue:" + queueId;
+    } finally {
+        return res;
+    }
+    */
+    
+    return event;
 })
