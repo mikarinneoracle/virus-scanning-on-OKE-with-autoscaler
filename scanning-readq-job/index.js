00782c045712e6dbc55e6b479ca4320679a8e380
Mika Rinne <mika.rinne@oracle.com>
2023-03-08 14:53:34 +0200

    fixes

--- scanning-readq-job/index.js
+++ scanning-readq-job/index.js
@@ -29,28 +29,36 @@ async function readQ() {
                 var cmd = "scan";
                 if(msg.content.includes("/")) {
                     console.log("Just deleting file " + msg.content);    
-                    cmd = "delete";
-                } else {
-                    console.log("Scanning " + msg.content);   
-                }
-                await scan("./scan.sh " + msg.content + " " + cmd, async function(error, stdout, stderr) {
-                    if (error || stderr) {
-                        console.log("Scan failed:");
-                        console.log( error ? error : "" + " " + stderr ? stderr : "");
-                    } else {
-                        console.log("Scan succesful:");
-                        console.log( stdout ? stdout : "");
-                    }
+                    await oClient.deleteObject(delObjReq).catch(async function(error) {
+                        console.log("Delete error");
+                        console.log(error);
+                    });
                     console.log("Deleting message from Q.");
-                    var delReq = {
-                          queueId: queueId,
-                          messageReceipt: msg.receipt
-                    };
                     await qClient.deleteMessage(delReq).catch(error => {
                         console.log(error);
                     });
                     readQ();
-                });
+                } else {
+                    console.log("Scanning " + msg.content);   
+                    await scan("./scan.sh " + msg.content, async function(error, stdout, stderr) {
+                        if (error || stderr) {
+                            console.log("Scan failed:");
+                            console.log( error ? error : "" + " " + stderr ? stderr : "");
+                        } else {
+                            console.log("Scan succesful:");
+                            console.log( stdout ? stdout : "");
+                        }
+                        console.log("Deleting message from Q.");
+                        var delReq = {
+                              queueId: queueId,
+                              messageReceipt: msg.receipt
+                        };
+                        await qClient.deleteMessage(delReq).catch(error => {
+                            console.log(error);
+                        });
+                        readQ();
+                    });
+                }
             });
         } else {
             console.log("Job finished from Q - Q empty. ");
